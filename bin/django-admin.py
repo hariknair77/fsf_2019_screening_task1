b9e0f101c83405a43b3e5bdadab142f454db5b80
hariknair77 <hariknair77@gmail.com>
2019-03-10 10:54:44 +0530

    setup environment

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/hari/Dev/django/fsf_2019_screening_task1/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
---
+++ lib/python3.7/posixpath.py
@@ -0,0 +1,1 @@
+/home/hari/anaconda3/lib/python3.7/posixpath.py
